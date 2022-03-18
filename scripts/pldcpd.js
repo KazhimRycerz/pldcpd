edc646511ca8c3b33d7dd46e3844f74e38211125
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-03-18 19:36:37 +0100

    Updates

--- scripts/pldcpd.js
+++ scripts/pldcpd.js
@@ -1,26 +1,29 @@
 "use strict";
 
 //Datenerfassung
-  let myKF = 120;
-  myKF += 12.5;
-  let myLF = 111;
-  let myPEX = 1524;
-  let myPED = 2400;
-  let myLP = 200;
-  let maKF = 120;
-  let maLF = 105;
-  let maPEX = 1950;
-  let maPED = 1500;
-  let maLP = 150;
-  let myLCoins = 150; 
-  myLCoins += 20;
-  const firstName = "Joachim";
-  const lastName = "Ritter";
-  let myCL = "IV";
-  
+let myKF = 120;
+myKF += 12.5;
+let myLF = 111;
+let myPEX = 1524;
+let myPED = 2400;
+let myLP = 200;
+let maKF = 120;
+let maLF = 105;
+let maPEX = 1950;
+let maPED = 1500;
+let maLP = 150;
+let myLCoins = 1500;
+myLCoins += 20;
+let firstName = "Joachim";
+let lastName = "Ritter";
+/* const firstName = "Joachim";
+  const lastName = "Ritter"; */
+let myCL = "IV";
+let boolVar = false;
+
 // Ende Datenerfassung
 
-/* function accountData() {
+function accountData() {
   document.getElementById("myKF").innerHTML = myKF;
   document.getElementById("myLF").innerHTML = myLF;
   document.getElementById("myPED").innerHTML = myPEX;
@@ -29,37 +32,26 @@
   document.getElementById("maLF").innerHTML = maLF;
   document.getElementById("maPED").innerHTML = maPEX;
   document.getElementById("maPEX").innerHTML = maPED;
-  document.getElementById("myLP").innerHTML = myLP;
-  document.getElementById("maLP").innerHTML = maLP;
-  document.getElementById("aside_Guthaben").innerHTML = myLCoins;
+  document.getElementById("aside_guthaben").innerHTML = myLCoins;
 }
-accountData(); */
+accountData();
 
+const aside = document.getElementById("aside_account");
 
+const hide_btn = document.getElementById("aside_h_Btn");
+const show_btn = document.getElementById("aside_s_Btn");
 
-
-
-
-
-  
-
-const aside = document.getElementById('aside_account');
-
-const hide_btn =document.getElementById('aside_h_Btn');
-const show_btn =document.getElementById('aside_s_Btn');
-
-const hide_account = () => {  
-  aside.style.display = 'none'; 
-  hide_btn.style.display = 'none';
-  show_btn.style.display = 'block'; 
-}
+const hide_account = () => {
+  aside.style.display = "none";
+  hide_btn.style.display = "none";
+  show_btn.style.display = "block";
+};
 
 const show_account = () => {
-  aside.style.display = 'block';
-  hide_btn.style.display = 'block';
-  show_btn.style.display = 'none';
-  
-}
+  aside.style.display = "block";
+  hide_btn.style.display = "block";
+  show_btn.style.display = "none";
+};
 
-hide_btn.addEventListener('click', hide_account);
-show_btn.addEventListener('click', show_account);
+hide_btn.addEventListener("click", hide_account);
+show_btn.addEventListener("click", show_account);
