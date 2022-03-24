772877eaf27a9ec96832bfc5a73a002984a313d3
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-03-24 20:23:57 +0100

    colors and smaller issues
    reduced codes - same effect

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # pldcpd
+
+![Screenshot from Project](./pldcpd.png)
+
+### About Project
+
+  International Plattform for coordinating CPD (Continuing Professional Development) for lighting designers, architects and engineers in architecture.
+
+  
+### How to use this Project
+
+lorem ........
+
+##### Developed With
+
+- [x] _HTML5_
+- [ ] _CSS3_
+- [ ] _SASS_
+- [x] _SCSS_
+- [x] _JavaScript_
+- [ ] _React_
+- [ ] _Bootstrap_
+- [x] _npm_
+- [ ] _..._
+
+---
+
+### Contact
+
+Mail: <jritter@via-internet.com><br>
+GitHub: [Joachim Ritter](https://github.com/KazhimRycerz)<br>
+LinkedIn: [Joachim Ritter](https://www.linkedin.com/in/joachimr/)<br>
+Xing: [Joachim Ritter](https://www.xing.com/profile/Joachim_Ritter19/)<br>
+Instagram: [Joachim Ritter](https://www.instagram.com/ritter_joachim/)<br>
+
+
+---
+
+### Used Tools
+
+- [ionicons](https://)
+- [Canva](https://www.canva.com/)
+- [npm](https://www.npmjs.com/)
+- [Google Fonts](https://fonts.google.com/)
+- [Visual Studio Code](https://code.visualstudio.com/)
+- [ColorZilla](https://www.colorzilla.com/chrome/)
+
+---
+
+© Joachim Ritter
---
+++ dist/scripts/index.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//Datenerfassung
+let myKF;
+myKF = 120;
+myKF += 12.5;
+let myLF = 111;
+let myPEX = 1524;
+let myPED = 2400;
+let myLP = 200;
+let maKF = 120;
+let maLF = 105;
+let maPEX = 1950;
+let maPED = 1500;
+let maLP = 150;
+let myLCoins = 1500;
+myLCoins += 20;
+let firstName = "Joachim";
+let lastName = "Ritter";
+/* const firstName = "Joachim";
+  const lastName = "Ritter"; */
+let myCL = "IV";
+let boolVar = false;
+
+// Ende Datenerfassung
+
+function accountData() {
+  document.getElementById("myKF").innerHTML = myKF;
+  document.getElementById("myLF").innerHTML = myLF;
+  document.getElementById("myPED").innerHTML = myPEX;
+  document.getElementById("myPEX").innerHTML = myPED;
+  document.getElementById("maKF").innerHTML = maKF;
+  document.getElementById("maLF").innerHTML = maLF;
+  document.getElementById("maPED").innerHTML = maPEX;
+  document.getElementById("maPEX").innerHTML = maPED;
+  document.getElementById("aside_guthaben").innerHTML = myLCoins;
+}
+accountData();
+
+const aside = document.getElementById("aside_account");
+
+const hide_btn = document.getElementById("aside_h_Btn");
+const show_btn = document.getElementById("aside_s_Btn");
+
+const hide_account = () => {
+  aside.style.display = "none";
+  hide_btn.style.display = "none";
+  show_btn.style.display = "block";
+};
+
+const show_account = () => {
+  aside.style.display = "block";
+  hide_btn.style.display = "block";
+  show_btn.style.display = "none";
+};
+
+hide_btn.addEventListener("click", hide_account);
+show_btn.addEventListener("click", show_account);
