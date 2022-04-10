b5d6692c1f4d0033cd79fa0bda25b9c213906760
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-10 21:04:58 +0200

    Updates

--- scripts/global.js
+++ scripts/global.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "-60px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "1vw";
-  }
+  document.getElementById("mySidenav").style.left = "0";
+  document.getElementById("main").style.marginLeft = "-60px";
+  /* document.body.style.backgroundColor = "rgba(0,0,0,0.4)"; */
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.left = "-150px";
+  document.getElementById("main").style.marginLeft = "1vw";
+  /* document.body.style.backgroundColor = "white"; */
+}
