3984dceed3bac09274b334b7597d618ad4cd968a
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-10 20:04:32 +0200

    burger menue

---
+++ src/scripts/global.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "-60px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "1vw";
+  }
