a52c009897bca1efd7ef696043bafe3a28917b9f
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-04-19 09:03:32 +0200

    optimisation

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,11 +1,11 @@
 function openNav() {
   document.getElementById("mySidenav").style.left = "0";
-  document.getElementById("main").style.marginLeft = "-100px";
+  document.getElementById("main").style.marginLeft = "-150px";
   /* document.body.style.backgroundColor = "rgba(0,0,0,0.4)"; */
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.left = "-200px";
+  document.getElementById("mySidenav").style.left = "-550px";
   document.getElementById("main").style.marginLeft = "1vw";
   /* document.body.style.backgroundColor = "white"; */
 }
