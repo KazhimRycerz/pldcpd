8c7a6dc2dff0cf0c0c03430877bbf79f74b9f21c
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-06-10 10:24:19 +0200

    erste Tests mit eventlistener

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -12,6 +12,10 @@ const closeNav = () => {
   document.getElementById("header").style.boxShadow = $boxshadow;
 }
 
+// const cClass =document.querySelectorAll("C");
+// cClass.forEach(elem => document.addEventListener('click', closeNav))
+// document.addEventListener('click', closeNav)
+
 // var f = document.getElementById('Punkt');
 // document.addEventListener('click', function(ev){
 //     f.style.transform = 'translateY('+(ev.clientY -25)+'px)';
@@ -20,14 +24,16 @@ const closeNav = () => {
 
 
 const stopRotate = (event) => {
-  const move = event.target;
-  const moveArea1 = document.querySelector("#header");
+  const move = event.currentTarget;
+  const moveArea1 = document.querySelector("header");
   const moveArea2 = document.querySelector("main");
   const moveArea3 = document.querySelector("aside");
   const rotaters = document.querySelectorAll(".rotate");
   console.log(move, moveArea1, moveArea2)
-  if (move == moveArea1 || move == moveArea2 || move == moveArea3) {rotaters.forEach(elem => elem.style.animationPlayState = "paused")}
+  if (move == moveArea1 || move == moveArea2 || move == moveArea3) 
+  {rotaters.forEach(elem => elem.style.animationPlayState = "paused")}
   else {rotaters.forEach(elem => elem.style.animationPlayState = "running")}
 }
 
+//capturing??
 document.addEventListener('mouseover', stopRotate);
