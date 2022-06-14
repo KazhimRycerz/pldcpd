00a126b61338855a4fec6b473346ba010bab0a0c
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-06-14 16:32:13 +0200

    Eventhandler Rotaters gemanaged

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -12,29 +12,24 @@ const closeNav = () => {
   document.getElementById("header").style.boxShadow = $boxshadow;
 }
 
-/* const stopRotate = (event) => {
-  const move = event.target;
-  const moveArea = document.querySelectorAll("main, main *, header, header *, aside, aside *");
-  const rotaters = document.querySelectorAll(".drehmoment *");
-  
-  moveArea.forEach(elemArea => {
-    if (elemArea == move)
-    {rotaters.forEach(elem => elem.style.animation = "paused"); 
-    console.log(elemArea, move)
-     } else {rotaters.forEach(elem => elem.style.animation = "running"); console.log("check", elemArea, move)}
-    })
-} */
 
 const stopRotate = (event) => {
   const move = event.target;
-  const moveArea1 = document.querySelector("header");
-  const moveArea2 = document.querySelector("main");
-  const moveArea3 = document.querySelector("aside");
+  
   const rotaters = document.querySelectorAll(".rotate");
   
-  if (move == moveArea1 || move == moveArea2 || move == moveArea3) {rotaters.forEach(elem => elem.style.animationPlayState = "paused")}
+  if (move.closest("header") || move.closest("main") || move.closest("aside")) {rotaters.forEach(elem => elem.style.animationPlayState = "paused")}
   else {rotaters.forEach(elem => elem.style.animationPlayState = "running")}
 }
 
-
 document.addEventListener('mouseover', stopRotate); 
+
+/* const manageMainNavigator = (event) => {
+  const navmain = document.querySelectorall("#navmain a, #btn_nav_close");
+  document.getElementById("btn_nav_open").style.display = "block";
+  document.getElementById("btn_nav_close").style.display = "none";
+  document.getElementById("navmain").style.top = "-200px";
+  document.getElementById("header").style.boxShadow = $boxshadow;
+
+}
+document.addEventListener('click', manageMainNavigator);  */
