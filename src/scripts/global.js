2f60aaeaa995a368b0017218d67748ad33887c58
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-06-16 20:19:16 +0200

    Navigation mit Toggle Button

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,38 +1,25 @@
+// mainnavigation über Toggle öffnen und schließen
 
+const burgerMenu = document.querySelector('#burger-menu');
+const navMain = document.querySelector('#navmain');
+let flag = false;
 
-// event open and close MainNavigation
-
-const openNav = () => {
-  openMainNav.style.display = "none";
-  closeMainNav.style.display = "block";
-  navMain.style.top = "100px";
-  navMain.style.transition = "0.5s ease-in-out";
-  homeAside.style.animation = "hide_home_aside"
+function navigationShow() {
+  burgerMenu.classList.toggle("changeBurger");
+  if ( flag ) {
+    navMain.classList.toggle("hideNav")
+  } else {
+    navMain.classList.toggle("showNav");
+    flag = true
+  }
+  //flag = ! flag;
 }
 
-const closeNav = () => {
-  openMainNav.style.display = "block";
-  closeMainNav.style.display = "none";
-  navMain.style.top = "-500px";
-  header.style.boxShadow = $boxshadow;
-}
-
-const openMainNav = document.querySelector("#btn_nav_open")
-openMainNav.addEventListener('click', openNav);
-
-const closeMainNav = document.querySelector('#btn_nav_close');
-closeMainNav.addEventListener('click', closeNav)
-
-const closingNav = document.querySelectorAll(".closebtn")
-closingNav.forEach(elem => elem.addEventListener('click', closeNav))
-
-const navMain = document.querySelector("#navmain")
-const header = document.querySelector("header")
-const homeAside = document.querySelector("home_aside_account")
+burgerMenu.addEventListener('click', navigationShow)
 
 
 
-// stop rotate event
+//______________stop rotate event_______________________________________//
 
 const stopRotate = (event) => {
   const move = event.target;
@@ -46,8 +33,7 @@ const stopRotate = (event) => {
 document.addEventListener('mouseover', stopRotate); 
 
 
-
-// show and Hide aside
+//______________show and Hide aside_______________________________________//
 const homeAsideAccount = document.querySelector("#home_aside_account");
 const hideButton = document.querySelector("#home_aside_h_Btn");
 const showButton = document.querySelector("#home_aside_s_Btn");
