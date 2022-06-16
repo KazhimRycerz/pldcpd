5ec6479abcfe14f8874a7c1bc747a922c8ffc1fb
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-06-16 20:20:45 +0200

    Updates

--- scripts/global.js
+++ scripts/global.js
@@ -1,45 +1,25 @@
-window.onload = function() {
-  init();
-  etwasAnderesTun();
-};
+// mainnavigation über Toggle öffnen und schließen
 
+const burgerMenu = document.querySelector('#burger-menu');
+const navMain = document.querySelector('#navmain');
+let flag = false;
 
-// event open and close Mainnavigation
-
-const openNav = () => {
-  openMainNav.style.display = "none";
-  closeMainNav.style.display = "block";
-  navMain.style.top = "100px";
-  navMain.style.transition = "0.3s ease-in-out";
-  //homeAside.style.animation = "hide_home_aside"
+function navigationShow() {
+  burgerMenu.classList.toggle("changeBurger");
+  if ( flag ) {
+    navMain.classList.toggle("hideNav")
+  } else {
+    navMain.classList.toggle("showNav");
+    flag = true
+  }
+  //flag = ! flag;
 }
 
-const closeNav = () => {
-  openMainNav.style.display = "block";
-  closeMainNav.style.display = "none";
-  navMain.style.top = "-200px";
-  header.style.boxShadow = $boxshadow;
-}
-
-const openMainNav = document.querySelector("#btn_nav_open")
-openMainNav.addEventListener('click', openNav);
-
-const closeMainNav = document.querySelector('#btn_nav_close');
-closeMainNav.addEventListener('click', closeNav)
-
-const closingNav = document.querySelectorAll(".closebtn")
-closingNav.forEach(elem => elem.addEventListener('click', closeNav))
-
-const navMain = document.querySelector("#navmain")
-const header = document.querySelector("header")
-const homeAside = document.querySelector("home_aside_account")
+burgerMenu.addEventListener('click', navigationShow)
 
 
 
-
-
-
-
+//______________stop rotate event_______________________________________//
 
 const stopRotate = (event) => {
   const move = event.target;
@@ -52,12 +32,37 @@ const stopRotate = (event) => {
 
 document.addEventListener('mouseover', stopRotate); 
 
-/* const manageMainNavigator = (event) => {
-  const navmain = document.querySelectorall("#navmain a, #btn_nav_close");
-  document.getElementById("btn_nav_open").style.display = "block";
-  document.getElementById("btn_nav_close").style.display = "none";
-  document.getElementById("navmain").style.top = "-200px";
-  document.getElementById("header").style.boxShadow = $boxshadow;
 
-}
-document.addEventListener('click', manageMainNavigator);  */
+//______________show and Hide aside_______________________________________//
+const homeAsideAccount = document.querySelector("#home_aside_account");
+const hideButton = document.querySelector("#home_aside_h_Btn");
+const showButton = document.querySelector("#home_aside_s_Btn");
+
+const hideAccount = () => {
+  homeAsideAccount.style.animation = "hide_home_aside 2s ease-in-out forwards";
+  showButton.style.animation = "moveButton 1s 0s ease-in-out forwards";
+  hideButton.style.animation = "moveButton 1s 0s ease-in-out forwards";
+  hideButton.style.display = "none";
+  showButton.style.display = "block";
+  Gruß_main_p.style.right = "3vw";
+  C1.style.left = "1.7vw"; //4
+  C1.style.width = "98%"; //4
+};
+
+const showAccount = () => {
+  hideButton.style.display = "block";
+  showButton.style.display = "none";
+  hideButton.style.animation = "moveButtonback 1s ease-in-out forwards";
+  homeAsideAccount.style.animation = "show_home_aside 2s ease-in-out forwards";
+  homeAsideAccount.style.display ="block"
+  Gruß_main_p.style.right = "10vw";
+  C1.style.left = "-1.5vw";
+  C1.style.width = "100%"; //4
+};
+
+hideButton.addEventListener("click", hideAccount);
+showButton.addEventListener("click", showAccount);
+
+window.onload = function() {
+  hideAccount()
+};
