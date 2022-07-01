d69e2a5302d7b4ff36b96e3b3f31417a2522f096
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-07-01 22:12:44 +0200

    Updates

--- scripts/daten.js
+++ scripts/daten.js
@@ -1,15 +1,54 @@
-let myKF; // individual KnowledgeFactor;
-let myLF; // individual LearningFactor;
-let myPEX; // individual Professional EXperience
-let myPED; // individual Professional EDucationb
-let myLP;   // individual LearningFactor
-let maKF; // durchschnittlicher KnowledgeFactor im Markt
-let maLF;
-let maPEX;
-let maPED;
-let maLP;
-let myLCoins;
-let firstName;
-let lastName;
-let myCL;
-let boolVar = false;
+const datenJR= {
+  myKF: 120,
+  myKF: 12.5,
+  myLF: 111,
+  myPEX: 1524,
+  myPED: 2400,
+  myLP: 200,
+  maKF: 120,
+  maLF: 105,
+  maPEX: 1950,
+  maPED: 1500,
+  maLP: 150,
+  myLCoins: 1500,
+  myLCoins: 20,
+  firstName: "Joachim",
+  lastName: "Ritter",
+  myCL: "IV",
+  boolVar: false}
+
+myKF = 120;
+myKF += 12.5;
+myLF = 111;
+myPEX = 1524;
+myPED = 2400;
+myLP = 200;
+maKF = 120;
+maLF = 105;
+maPEX = 1950;
+maPED = 1500;
+maLP = 150;
+myLCoins = 1500;
+myLCoins += 20;
+firstName = "Joachim";
+lastName = "Ritter";
+myCL = "IV";
+boolVar = false;
+
+// Ende Datenerfassung
+
+const accountData =()=> {
+  document.getElementById("myKF").innerHTML = myKF;
+  document.getElementById("myLF").innerHTML = myLF;
+  document.getElementById("myPED").innerHTML = myPEX;
+  document.getElementById("myPEX").innerHTML = myPED;
+  document.getElementById("maKF").innerHTML = maKF;
+  document.getElementById("maLF").innerHTML = maLF;
+  document.getElementById("maPED").innerHTML = maPEX;
+  document.getElementById("maPEX").innerHTML = maPED;
+  document.getElementById("aside_guthaben").innerHTML = myLCoins;
+}
+accountData();
+
+
+//export{accountData, datenJR}
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,7 @@
 "use strict";
 
-myKF = 120;
-myKF += 12.5;
-myLF = 111;
-myPEX = 1524;
-myPED = 2400;
-myLP = 200;
-maKF = 120;
-maLF = 105;
-maPEX = 1950;
-maPED = 1500;
-maLP = 150;
-myLCoins = 1500;
-myLCoins += 20;
-firstName = "Joachim";
-lastName = "Ritter";
-/* const firstName = "Joachim";
-  const lastName = "Ritter"; */
-myCL = "IV";
-boolVar = false;
+//import {daten} from "../modules/daten.js"
 
-// Ende Datenerfassung
 
-const accountData =()=> {
-  document.getElementById("myKF").innerHTML = myKF;
-  document.getElementById("myLF").innerHTML = myLF;
-  document.getElementById("myPED").innerHTML = myPEX;
-  document.getElementById("myPEX").innerHTML = myPED;
-  document.getElementById("maKF").innerHTML = maKF;
-  document.getElementById("maLF").innerHTML = maLF;
-  document.getElementById("maPED").innerHTML = maPEX;
-  document.getElementById("maPEX").innerHTML = maPED;
-  document.getElementById("aside_guthaben").innerHTML = myLCoins;
-}
-accountData();
 
 
