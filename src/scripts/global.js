4505b57014528f989c19fd2febff6db5a3b9e0cf
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2022-07-07 16:47:22 +0200

    Development jsut before solving button

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -36,75 +36,83 @@ document.addEventListener("mouseover", stopRotate);
 //______________onload_______________________________________//
 
 /* window.onload = function() {
-
+  
 }; */
 
-//______________Login ==> show account button_______________________________________//
-
-const  logInExecute =  () => {
-    //console.log(logInOut.textContent);
-  if (logInOut.textContent == "log me in ") {
-    
-    buttonShowAccount.style.zIndex = "0";
-    buttonShowAccount.style.display = "inline-block";
-    user.style.display= "inline-block";
-    logInOut.textContent = "log me out ";
-  } else if (logInOut.textContent == "log me out ") {
-    buttonShowAccount.style.animation ="moveButtonBack 1s ease-in-out forwards";
-    buttonShowAccount.textContent = "show account";
-    buttonShowAccount.style.zIndex = "0";
-    buttonShowAccount.style.display = "none";
-    homeAsideAccount.style.animation =
-    "hide_home_aside 2s ease-in-out forwards";
-    gruß.style.right = "4vw"; 
-    C1.style.left = "0px";
-    user.style.display= "none";
-    logInOut.textContent = "log me in ";
-  } else {alert("Ein Fehler ist aufgetreten")}
-};
-
-//______________show account and move Button_______________________________________//
-
-const showAccountHome = () => {
+const moveAccountAndButton = () => {
   if (buttonShowAccount.textContent == "show account") {
-    buttonShowAccount.style.animation = "moveButtonback 1s ease-in-out forwards";
-    homeAsideAccount.style.animation = "show_home_aside 2s ease-in-out forwards";
-    homeAsideAccount.style.display = "block";
-    gruß.style.right = "10vw";
-    C1.style.left = "-2.5vw";
-    buttonShowAccount.textContent = "hide account";
-    console.log("Ergebnis if")
-  } else if (buttonShowAccount.textContent == "hide account"){
-    homeAsideAccount.style.animation = "hide_home_aside 2s ease-in-out forwards";
-    buttonShowAccount.style.animation = "moveButton 1s 0s ease-in-out forwards";
-    gruß.style.right = "4vw";
-    C1.style.left = "0px"; //4
-    C1.style.margin = "60px auto"; //4
-    buttonShowAccount.textContent = "show account";
-    console.log("Ergebnis else if")
-  } else {alert("Ein Fehler ist aufgetreten")}
+    showAccount();
+  } else if (buttonShowAccount.textContent == "hide account") {
+    hideAccount();
+  } else {
+    alert("Ein Fehler ist aufgetreten");
+  }
 };
 
+const showAccount = () => {
+  buttonShowAccount.style.animation = "moveButtonback 1s ease-in-out forwards";
+  homeAsideAccount.style.animation = "show_home_aside 2s ease-in-out forwards";
+  homeAsideAccount.style.display = "block";
+  gruß.style.right = "10vw";
+  C1.style.left = "-2.5vw";
+  buttonShowAccount.textContent = "hide account";
+};
+const hideAccount = () => {
+  homeAsideAccount.style.animation = "hide_home_aside 2s ease-in-out forwards";
+  buttonShowAccount.style.animation = "moveButton 1s 0s ease-in-out forwards";
+  gruß.style.right = "4vw";
+  C1.style.left = "0px"; //4
+  C1.style.margin = "60px auto"; //4
+  buttonShowAccount.textContent = "show account";
+};
+
+//______________Login ==> manage account button_______________________________________//
+
+const handleButton = () => {
+  if (flagButton && buttonShowAccount.textContent == "show account") {
+    console.log(
+      flagButton,
+      logInOut.textContent,
+      buttonShowAccount.textContent
+    );
+    buttonShowAccount.classList.replace("hideBut", "showBut");
+    logInOut.textContent = "log me out ";
+    //user.style.display= "inline-block";
+    flagButton = false;
+  } else if (!flagButton && buttonShowAccount.textContent == "show account") {
+    console.log(
+      flagButton,
+      logInOut.textContent,
+      buttonShowAccount.textContent
+    );
+    buttonShowAccount.classList.replace("showBut", "hideBut");
+    logInOut.textContent = "log me in ";
+    //user.style.display= "none";
+    flagButton = true;
+  } else if (!flagButton && buttonShowAccount.textContent == "hide account") {
+    const promise = new Promise((resolve, reject) => {
+      resolve(hideAccount());
+    });
+    promise.then((result) => handleButton());
+    console.log(
+      "test",
+      flagButton,
+      logInOut.textContent,
+      buttonShowAccount.textContent
+    );
+  } else {
+    alert("Ein Fehler ist aufgetreten");
+  }
+  console.log(flagButton, logInOut.textContent, buttonShowAccount.textContent);
+};
+
+let flagButton = true;
 const homeAsideAccount = document.querySelector("#home_aside_account");
 const showLogin = document.querySelector("#showlogin");
-showLogin.addEventListener("click", logInExecute);
 const logInOut = document.querySelector("#logInOut");
 const buttonShowAccount = document.querySelector("#buttonShowAccount");
-buttonShowAccount.addEventListener("click", showAccountHome);
-console.log(buttonShowAccount.textContent)
 const gruß = document.querySelector("#Gruß_main_p");
 const C1 = document.querySelector("#C1");
 const user = document.querySelector("#user");
-const moveButtonBack = document.querySelector("moveButtonback");
-
-
-/* function navigationShow() {
-  burgerMenu.classList.toggle("changeBurger");
-  if (flag) {
-    navMain.classList.toggle("hideNav");
-  } else {
-    navMain.classList.toggle("showNav");
-    flag = true;
-  }
-  //flag = ! flag;
-} */
+showLogin.addEventListener("click", handleButton);
+buttonShowAccount.addEventListener("click", moveAccountAndButton);
