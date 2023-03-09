7aad1f96c5a532b08288c1fb6451462cd4178548
Joachim Ritter <95426840+KazhimRycerz@users.noreply.github.com>
2023-03-09 11:31:21 +0100

    preparation for mergin

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,9 +16,6 @@ const userSchema = mongoose.Schema({
       unique: true,
       trim: true
    },
-   carrerPath: [{
-      type: 
-   }]
    createdOn:{
       type: Date,
       immutable: true,
